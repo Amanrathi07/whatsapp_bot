162911e6ea7b80bdf9ec80f2afbeee69a9e6f016
Amanrathi07 <amanrathi4321@gmail.com>
2026-07-04 23:17:04 +0530

    env variable added

--- src/massages/responce.ts
+++ src/massages/responce.ts
@@ -3,7 +3,8 @@ import twilio from "twilio";
 
 
 const client = twilio(
-  
+  process.env.TWILIO_ACCOUNT_SID,
+  process.env.TWILIO_AUTH_TOKEN
 );
 
 export async function sendResponse(to: string, body: string) {
